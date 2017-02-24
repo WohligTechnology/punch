1cb019d551254e2610de4703316d695ecb52b7c7
pratikg17 <pratikg17.wohlig@gmail.com>
2017-02-24 13:21:05 +0530

    changes in give

--- js/controllers.js
+++ js/controllers.js
@@ -63,12 +63,35 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
 
 
   })
-  .controller('GiveCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+  .controller('GiveCtrl', function($scope, TemplateService, NavigationService, $timeout, $uibModal) {
     //Used to name the .html file
     $scope.template = TemplateService.changecontent("give");
     $scope.menutitle = NavigationService.makeactive("Give");
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
+     $scope.validEmail = /^[a-z]+[@][a-z]+[.]+[a-z]*$/;
+      $scope.subscribeForm ={};
+
+       $scope.thanks = function() {
+        $uibModal.open({
+            animation: true,
+            templateUrl: "views/modal/thank.html",
+            scope: $scope
+
+        })
+    };
+      $scope.subscribe=function(email){
+        console.log("email",email);
+        if(email){
+          $scope.show = true;
+            $scope.thanks();
+            $timeout(function() {
+                        $scope.show = false;
+                        $scope.subscribeForm = {};
+                    }, 2000);
+        }
+        
+      } 
 
   })
   .controller('ShantilalCtrl', function($scope, TemplateService, NavigationService, $timeout) {
